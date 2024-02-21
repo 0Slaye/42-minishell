e8b64150fadf827e14e1c0c7af9cd7a5ffba00db
0Slaye <uduterrage@gmail.com>
2024-02-21 19:12:53 +0100

    First push

---
+++ minishell/sources/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 19:12:26 by uwywijas          #+#    #+#             */
+/*   Updated: 2024/02/21 19:12:36 by uwywijas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "commons.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	(void) argc;
+	(void) argv;
+	(void) env;
+	printf("Minishell: working.\n");
+	return (0);
+}
