99300af2941e1a043e2d2131a2422c109f4865e4
0Slaye <uduterrage@gmail.com>
2024-03-07 12:20:42 +0100

    jeu. 07 mars 2024 12:20:42 CET

--- minishell/sources/prompt/prompt.c
+++ minishell/sources/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 14:33:18 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/03/07 11:16:57 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/03/07 12:17:44 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ void	prompt(char *value, t_input *input)
 {
 	char	*line_read;
 
-	int i = -1;
-	while (++i != 1)
+	while (TRUE)
 	{
 		line_read = readline(value);
 		add_history(line_read);
