661965fcd62f11e9093a4380b1b7846ac407c27d
0Slaye <uduterrage@gmail.com>
2024-03-08 17:44:57 +0100

    ven. 08 mars 2024 17:44:57 CET

--- minishell/sources/lexer/lexer_formater.c
+++ minishell/sources/lexer/lexer_formater.c
@@ -6,7 +6,7 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 14:18:42 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/03/08 17:42:29 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/03/08 17:44:39 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,31 +24,6 @@ void	redirections_case(t_list **lexer)
 		if (lexer_get_type((*lexer)->next) == T_SR_REDIRECTION)
 			lst_update_type(*lexer, (*lexer)->next, T_DR_REDIRECTION);
 	}
-}
-
-int	quotes_case(t_list **lexer)
-{
-	char	*result;
-	char	*value;
-	int		i;
-	int		offset;
-
-	value = lexer_get_value(*lexer);
-	result = ft_calloc(ft_strlen(value) + 1, sizeof(char));
-	if (!result)
-		return (1);
-	i = -1;
-	offset = -1;
-	while (value[++i] != '\0')
-	{
-		if (quotes_case_utils_hashmap(value, result, &i, &offset) != 0)
-			return (free(result), 1);
-		if (quotes_case_util(value, result, &i, &offset) != 0)
-			break ;
-	}
-	result[++offset] = '\0';
-	((t_token *)(*lexer)->content)->value = result;
-	return (free(value), 0);
 }
 
 int	lexer_formater(t_list **lexer)
--- minishell/sources/lexer/lexer_formater_utils.c
+++ minishell/sources/lexer/lexer_formater_utils.c
@@ -6,7 +6,7 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 15:37:20 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/03/08 17:42:19 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/03/08 17:44:47 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,47 +20,4 @@ void	lst_update_type(t_list *first, t_list *second, int type)
 	ft_lstdelone(second, &lfree);
 	((t_token *) first->content)->type = type;
 	first->next = holder;
-}
-
-int	quotes_case_utils_hashmap(char *value, char *result, int *i, int *offset)
-{
-	int	*hashmap;
-
-	hashmap = ft_hashmap(value);
-	if (!hashmap)
-		return (1);
-	if (*i == (int) ft_strlen(value))
-		return (free(hashmap), 0);
-	if (hashmap[*i] == T_S_QUOTE)
-	{
-		if (ft_strchr(&value[*i + 1], S_QUOTE) == 0)
-			return (free(hashmap), 0);
-		result[++(*offset)] = value[*i];
-		while (hashmap[++(*i)] != T_S_QUOTE)
-			result[++(*offset)] = value[*i];
-		result[++(*offset)] = value[*i];
-	}
-	else if (hashmap[*i] == T_D_QUOTE)
-	{
-		if (ft_strchr(&value[*i + 1], T_D_QUOTE) == 0)
-			return (free(hashmap), 0);
-		result[++(*offset)] = value[*i];
-		while (hashmap[++(*i)] != T_D_QUOTE)
-			result[++(*offset)] = value[*i];
-		result[++(*offset)] = value[*i];
-	}
-	return (free(hashmap), 0);
-}
-
-int	quotes_case_util(char *value, char *result, int *i, int *offset)
-{
-	if (value[*i + 1] == '\0')
-		return (result[++(*offset)] = value[*i], 1);
-	if (value[*i] == S_QUOTE && value[*i + 1] == S_QUOTE)
-		*i += 1;
-	else if (value[*i] == D_QUOTE && value[*i + 1] == D_QUOTE)
-		*i += 1;
-	else
-		result[++(*offset)] = value[*i];
-	return (0);
 }
