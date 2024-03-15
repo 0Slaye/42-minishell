17a5c208c2cd2f6425cbe3277ed18737e0eb589e
0Slaye <uduterrage@gmail.com>
2024-03-15 17:06:50 +0100

    Update: lexer.c cleaning quotes

--- minishell/sources/lexer/lexer_formater.c
+++ minishell/sources/lexer/lexer_formater.c
@@ -6,7 +6,7 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 14:18:42 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/03/15 16:55:29 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/03/15 17:01:29 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,8 @@ int	lexer_formater(t_list **lexer)
 		if ((*lexer)->next)
 			redirections_case(lexer);
 		if (lexer_get_type(*lexer) == T_WORD && useless_quotes_case(lexer) != 0)
+			return (1);
+		if (lexer_get_type(*lexer) == T_WORD && clean_quotes_case(lexer) != 0)
 			return (1);
 		if (lexer_get_type(*lexer) == T_WORD )
 			update_token_by_ascii(lexer_get_value(*lexer));
