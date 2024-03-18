f2b9f224dede5f3f3e5a58d7709f337fb3afc1fa
0Slaye <uduterrage@gmail.com>
2024-03-18 15:08:19 +0100

    Update: commons.h functions order

--- minishell/includes/commons.h
+++ minishell/includes/commons.h
@@ -6,7 +6,7 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 19:12:23 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/03/18 15:02:00 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/03/18 15:07:35 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,34 +35,34 @@ typedef struct s_input
 void	prompt(char *value, t_input *input);
 
 // Lexer
+t_token	*get_token(int type, char *value);
 t_list	**lexer(char *line, t_input *input);
+t_list	**split_command(char *command);
+void	lst_update_type(t_list *first, t_list *second, int type);
+void	free_lexer(t_list **lexer);
+void	lfree(void *token);
+void	show_lexer(t_list **lexer);
+void	show_hashmap(int *hashmap, int length);
+void	setup_tokens(char *line, int *hashmap);
+void	replace_token_by_ascii(char *value);
+void	update_token_by_ascii(char *value);
+char	*lexer_get_value(t_list *list);
+char	*get_word(int *hashmap, char *line);
+char	*lexer_expender(char *value, char **envp);
+char	*get_expend_value(char *value, char **envp);
 int		lexer_get_type(t_list *list);
 int		lexer_formater(t_list **lexer);
 int		spaces_before_case(t_list **lexer);
 int		spaces_after_case(t_list **lexer);
 int		useless_quotes_case(t_list **lexer);
-char	*lexer_expender(char *value, char **envp);
-void	lst_update_type(t_list *first, t_list *second, int type);
-char	*lexer_get_value(t_list *list);
-t_token	*get_token(int type, char *value);
-char	*get_word(int *hashmap, char *line);
-void	free_lexer(t_list **lexer);
-void	lfree(void *token);
-void	show_lexer(t_list **lexer);
 int		*ft_hashmap(char *line);
-void	show_hashmap(int *hashmap, int length);
 int		setup_quotes(char *line, int *hashmap);
-void	setup_tokens(char *line, int *hashmap);
-t_list	**split_command(char *command);
 int		get_list_value_length(t_list **list);
 int		clean_quotes_case(t_list **lexer);
 int		is_simple_quoted(char *value, int *hashmap, int is_quoted, int i);
 int		add_to_list(t_list **list, char *value, int type);
 int		add_var_to_list(t_list **list, char *value, char **envp, int *i);
-char	*get_expend_value(char *value, char **envp);
 int		list_return_selector(t_list **list);
-void	replace_token_by_ascii(char *value);
-void	update_token_by_ascii(char *value);
 int		get_envp_var_length(char *str);
 
 #endif
--- minishell/includes/errors.h
+++ minishell/includes/errors.h
@@ -6,13 +6,13 @@
 /*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 17:34:17 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/03/06 17:36:09 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/03/18 15:05:39 by uwywijas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
 
-# define ER_LEXER_QUOTE "lexer: Error when parsing for quotes\n"
+# define ER_LEXER_QUOTE "minishell: lexer: Error when parsing for quotes\n"
 
 #endif
