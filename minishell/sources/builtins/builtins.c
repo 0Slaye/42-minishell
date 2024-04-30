37d831e43b18d5a430917dbbdedc4be461760123
Thibault Al Yafi <tal-yafi@k0r3p13.42mulhouse.fr>
2024-04-30 18:11:38 +0200

    Unset doesn't delete but finds the right element | Alphabetical sort on export tbd

--- minishell/sources/builtins/builtins.c
+++ minishell/sources/builtins/builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tal-yafi <tal-yafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 18:04:48 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/04/22 14:31:26 by uwywijas         ###   ########.fr       */
+/*   Updated: 2024/04/30 17:43:37 by tal-yafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	check_and_exec_builtin(t_program *program, t_tree *node)
 	if (ft_strncmp(node->value, "export", 6) == 0)
 		return (ft_export(program, node), free_exit(program, EXIT_SUCCESS));
 	if (ft_strncmp(node->value, "unset", 5) == 0)
-		return (printf("%s\n", node->value), free_exit(program, EXIT_SUCCESS));
+		return (ft_unset(program, node), free_exit(program, EXIT_SUCCESS));
 	if (ft_strncmp(node->value, "env", 3) == 0)
 		return (ft_env(program), free_exit(program, EXIT_SUCCESS));
 	if (ft_strncmp(node->value, "exit", 4) == 0)
--- minishell/sources/builtins/unset.c
+++ minishell/sources/builtins/unset.c
@@ -6,30 +6,79 @@
 /*   By: tal-yafi <tal-yafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:45:37 by uwywijas          #+#    #+#             */
-/*   Updated: 2024/04/23 14:30:32 by tal-yafi         ###   ########.fr       */
+/*   Updated: 2024/04/30 18:10:53 by tal-yafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "commons.h"
 #include "errors.h"
+// Finds the right string but doesn't skip it
+// Delete it | don't add it to the new_e
+static void	ft_smaller_env(t_program *program, int pos)
+{
+	char	**new_e;
+	int		j;
+	
+	printf("%d\n", ft_array_len(program->envp));
+	new_e = ft_calloc(ft_array_len(program->envp), sizeof(char *));
+	if (!new_e)
+		return ((void)ft_putendl_fd(ER_MALLOC_FUNC, 2));
+	j = 0;
+	while (j < ft_array_len(program->envp) && j != pos)
+	{
+		new_e[j] = ft_strdup(program->envp[j]);
+		if (!new_e[j])
+		{
+			ft_array_cleaner((void **)new_e, j);
+			return ((void)ft_putendl_fd(ER_MALLOC_FUNC, 2));
+		}
+		j++;
+	}
+	if (j == pos)
+		printf("%d | %s\n", j, program->envp[j]);
+	{
+		while (j < ft_array_len(program->envp) && program->envp[j + 1])
+		{
+			new_e[j] = ft_strdup(program->envp[j + 1]);
+			if (!new_e[j])
+			{
+				ft_array_cleaner((void **)new_e, j);
+				return ((void)ft_putendl_fd(ER_MALLOC_FUNC, 2));
+			}
+			j++;
+		}
+	}
+	while (j < ft_array_len(program->envp))
+	{
+		new_e[j] = NULL;
+		j++;
+	}
+	ft_array_cleaner((void **)program->envp, j);
+	program->envp = new_e;
+}
 
 void	ft_unset(t_program *program, t_tree *node)
 {
-	(void) program;
-	(void) node;
-	// char	**argv;
-	// int		i;
+	char	**argv;
+	int		i;
+	int		pos;
+	int		size;
 
-	// argv = get_cmd_option(node);
-	// if (!argv)
-	// 	return ;
-	// while (argv[i])
-	// 	i++;
-	// if (i == 1)
-	// 	return ; // add \n ?
-	// if (i > 1)
-	// {
-		
-	// } 
-	
+	argv = get_cmd_option(node);
+	if (!argv)
+		return ;
+	i = 1;
+	while (argv[i])
+	{
+		size = ft_strlen(argv[i]);
+		pos = 0;
+		while (program->envp[pos])
+		{
+			if (ft_strncmp(argv[i], program->envp[pos], size) == 0)
+				ft_smaller_env(program, pos);
+			pos++;
+		}
+		i++;
+	}
+	return (free(argv));
 }
